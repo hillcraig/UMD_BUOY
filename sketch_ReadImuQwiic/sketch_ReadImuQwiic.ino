32998872d706a20758465546eea089be40d95922
Reagan Nguyen <reagannguyen92@gmail.com>
2023-03-26 15:55:11 -0500

    IMU tester using the QWiic mux for addition for using scaled formatted ints

---
+++ sketch_ReadImuQwiic/sketch_ReadImuQwiic.ino
@@ -0,0 +1,306 @@
+/**
+ * Qwiic Mux Demo
+ *
+ */
+#include "ICM_20948.h" // Click here to get the library: http://librarymanager/All#SparkFun_ICM_20948_IMU
+#include <Wire.h>
+#define SERIAL_PORT Serial
+#define LED 4
+
+// Parameters
+const int MUX_ADDR = 0x70;
+const int IMU_CHANNEL = 0;
+const int NIR_CHANNEL = 1;
+
+// Pins
+const int PIN_RESET = 9;
+const int DC_JUMPER = 1;
+
+#define WIRE_PORT Wire // Your desired Wire port.      Used when "USE_SPI" is not defined
+#define AD0_VAL 1      // The value of the last bit of the I2C address.                \
+                       // On the SparkFun 9DoF IMU breakout the default is 1, and when \
+                       // the ADR jumper is closed the value becomes 0
+// Constants
+
+// Globals
+ICM_20948_I2C myICM;
+
+void setup() {
+
+  // Initialize communcation ports
+  Serial.begin(115200);
+  Wire.begin();
+
+
+
+  // Initialize visible spectral sensor
+  enableMuxPort(IMU_CHANNEL); 
+  myICM.begin();
+  disableMuxPort(IMU_CHANNEL);
+
+    pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
+
+  bool initialized = false;
+  while (!initialized)
+  {
+    enableMuxPort(0);
+/*#ifdef USE_SPI
+    myICM.begin(CS_PIN, SPI_PORT, SPI_FREQ); // Here we are using the user-defined SPI_FREQ as the clock speed of the SPI bus
+#else
+    myICM.begin(WIRE_PORT, AD0_VAL);
+#endif*/
+    myICM.begin(Wire, 1);
+    Serial.print(F("Initialization of the sensor returned: "));
+    Serial.println(myICM.statusString());
+    if (myICM.status != ICM_20948_Stat_Ok)
+    {
+      Serial.println("Trying again...");
+      delay(500);
+      digitalWrite(LED, LOW);
+    }
+    else
+    {
+      digitalWrite(LED, HIGH);
+      initialized = true;
+      Serial.println("Device Connected!");
+    }
+    disableMuxPort(0);
+  }
+}
+
+
+
+void loop() {
+  enableMuxPort(0);
+  if (myICM.dataReady())
+  {
+    myICM.getAGMT();         // The values are only updated when you call 'getAGMT'
+    convertRaw(myICM.agmt);
+                                 //printRawAGMT( myICM.agmt );     // Uncomment this to see the raw values, taken directly from the agmt structure
+    printScaledAGMT(&myICM); // This function takes into account the scale settings from when the measurement was made to calculate the values with units
+    delay(30);
+  }
+  else
+  {
+    SERIAL_PORT.println("Waiting for data");
+    delay(500);
+  }
+
+
+
+  disableMuxPort(0);
+
+}
+
+
+
+//Enables a specific port number
+boolean enableMuxPort(byte portNumber)
+{
+  if(portNumber > 7) portNumber = 7;
+
+  //Read the current mux settings
+  Wire.requestFrom(MUX_ADDR, 1);
+  if(!Wire.available()) return(false); //Error
+  byte settings = Wire.read();
+
+  //Set the wanted bit to enable the port
+  settings |= (1 << portNumber);
+ 
+  Wire.beginTransmission(MUX_ADDR);
+  Wire.write(settings);
+  Wire.endTransmission();
+
+  return(true);
+}
+
+//Disables a specific port number
+boolean disableMuxPort(byte portNumber)
+{
+  if(portNumber > 7) portNumber = 7;
+
+  //Read the current mux settings
+  Wire.requestFrom(MUX_ADDR, 1);
+  if(!Wire.available()) return(false); //Error
+  byte settings = Wire.read();
+
+  //Clear the wanted bit to disable the port
+  settings &= ~(1 << portNumber);
+
+  Wire.beginTransmission(MUX_ADDR);
+  Wire.write(settings);
+  Wire.endTransmission();
+
+  return(true);
+}
+
+// Below here are some helper functions to print the data nicely!
+
+void printPaddedInt16b(int16_t val)
+{
+  if (val > 0)
+  {
+    SERIAL_PORT.print(" ");
+    if (val < 10000)
+    {
+      SERIAL_PORT.print("0");
+    }
+    if (val < 1000)
+    {
+      SERIAL_PORT.print("0");
+    }
+    if (val < 100)
+    {
+      SERIAL_PORT.print("0");
+    }
+    if (val < 10)
+    {
+      SERIAL_PORT.print("0");
+    }
+  }
+  else
+  {
+    SERIAL_PORT.print("-");
+    if (abs(val) < 10000)
+    {
+      SERIAL_PORT.print("0");
+    }
+    if (abs(val) < 1000)
+    {
+      SERIAL_PORT.print("0");
+    }
+    if (abs(val) < 100)
+    {
+      SERIAL_PORT.print("0");
+    }
+    if (abs(val) < 10)
+    {
+      SERIAL_PORT.print("0");
+    }
+  }
+  SERIAL_PORT.print(abs(val));
+}
+
+void printRawAGMT(ICM_20948_AGMT_t agmt)
+{
+  SERIAL_PORT.print("RAW. Acc [ ");
+  printPaddedInt16b(agmt.acc.axes.x);
+  SERIAL_PORT.print(", ");
+  printPaddedInt16b(agmt.acc.axes.y);
+  SERIAL_PORT.print(", ");
+  printPaddedInt16b(agmt.acc.axes.z);
+  SERIAL_PORT.print(" ], Gyr [ ");
+  printPaddedInt16b(agmt.gyr.axes.x);
+  SERIAL_PORT.print(", ");
+  printPaddedInt16b(agmt.gyr.axes.y);
+  SERIAL_PORT.print(", ");
+  printPaddedInt16b(agmt.gyr.axes.z);
+  SERIAL_PORT.print(" ], Mag [ ");
+  printPaddedInt16b(agmt.mag.axes.x);
+  SERIAL_PORT.print(", ");
+  printPaddedInt16b(agmt.mag.axes.y);
+  SERIAL_PORT.print(", ");
+  printPaddedInt16b(agmt.mag.axes.z);
+  SERIAL_PORT.print(" ], Tmp [ ");
+  printPaddedInt16b(agmt.tmp.val);
+  SERIAL_PORT.print(" ]");
+  SERIAL_PORT.println();
+}
+void convertRaw(ICM_20948_AGMT_t agmt){
+  //constants
+  int s2g = 16384;
+  int s4g = 8192;
+  int s16g = 2048;
+  float gScale = 131.072;
+  float mScale = 0.15;
+  float g = 0.00981;  //milli g to m/s ^2 
+
+  //acc
+  agmt.acc.axes.x /= s2g;
+  agmt.acc.axes.y /= s2g;
+  agmt.acc.axes.z /= s2g;
+  agmt.gyr.axes.x /= gScale;
+  agmt.gyr.axes.y /= gScale;
+  agmt.gyr.axes.z /= gScale;
+  agmt.mag.axes.x *= mScale;
+  agmt.mag.axes.y *= mScale;
+  agmt.mag.axes.z *= mScale;
+  SERIAL_PORT.print(agmt.mag.axes.z);
+  delay(1000);
+}
+
+
+void printFormattedFloat(float val, uint8_t leading, uint8_t decimals)
+{
+  float aval = abs(val);
+  if (val < 0)
+  {
+    SERIAL_PORT.print("-");
+  }
+  else
+  {
+    SERIAL_PORT.print(" ");
+  }
+  for (uint8_t indi = 0; indi < leading; indi++)
+  {
+    uint32_t tenpow = 0;
+    if (indi < (leading - 1))
+    {
+      tenpow = 1;
+    }
+    for (uint8_t c = 0; c < (leading - 1 - indi); c++)
+    {
+      tenpow *= 10;
+    }
+    if (aval < tenpow)
+    {
+      SERIAL_PORT.print("0");
+    }
+    else
+    {
+      break;
+    }
+  }
+  if (val < 0)
+  {
+    SERIAL_PORT.print(-val, decimals);
+  }
+  else
+  {
+    SERIAL_PORT.print(val, decimals);
+  }
+}
+
+#ifdef USE_SPI
+void printScaledAGMT(ICM_20948_SPI *sensor)
+{
+#else
+void printScaledAGMT(ICM_20948_I2C *sensor)
+{
+#endif
+  SERIAL_PORT.print("Scaled. Acc (mg) [ ");
+  printFormattedFloat(sensor->accX(), 5, 2);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->accY(), 5, 2);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->accZ(), 5, 2);
+  SERIAL_PORT.print(" ], Gyr (DPS) [ ");
+  printFormattedFloat(sensor->gyrX(), 5, 2);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->gyrY(), 5, 2);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->gyrZ(), 5, 2);
+  SERIAL_PORT.print(" ], Mag (uT) [ ");
+  printFormattedFloat(sensor->magX(), 5, 2);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->magY(), 5, 2);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->magZ(), 5, 2);
+  SERIAL_PORT.print(" ], Tmp (C) [ ");
+  printFormattedFloat(sensor->temp(), 5, 2);
+  SERIAL_PORT.print(" ]");
+  SERIAL_PORT.println();
+}
+
+
